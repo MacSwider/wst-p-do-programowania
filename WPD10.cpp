455ef2be713d71f197c6a4add66ca2cdd8f22052
MacSwider <115549720+MacSwider@users.noreply.github.com>
2022-12-16 10:56:52 +0100

    Add files via upload

---
+++ WPD10.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+void zadanie_1()
+{
+
+}
+
+
+int main()
+{
+	int value,arg=1,sum_temp, sum_total=0;
+	cout << "Insert value of n: ";
+	cin >> value;
+	
+	for (int i = arg; i <= value; i++)
+	{
+		cout << "n"<<i<< "="<<i * (1 + i) / 2 << endl;
+		sum_temp = i * (1 + i) / 2;
+		sum_total = sum_total + sum_temp;
+	}
+	cout << "The sum of all results is: " << sum_total;
+}
+
